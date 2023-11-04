b7bdee2fd3dbdca8c34240c91f85007f0967555e
LyndaATT <59966456+LyndaATT@users.noreply.github.com>
2023-11-04 18:43:23 +0100

    separate artists/songs in tooltip + add dragging

--- public/visualisations/map/index.js
+++ public/visualisations/map/index.js
@@ -1,146 +1,199 @@
-//selecting map container element
-const svgContainer = d3.select("#map-container");
-const width = 1200;
-const height = 900; 
+//in addition to course, the followed tutorials are:
+//https://d3-graph-gallery.com/choropleth.html
+//https://www.javascripttutorial.net/javascript-dom/document-object-model-in-javascript/
+//https://d3-graph-gallery.com/graph/interactivity_tooltip.html
+//https://www.javascripttutorial.net/javascript-dom/javascript-mouse-events/
+//https://www.javascripttutorial.net/web-apis/javascript-drag-and-drop/
 
+//selecting map container element
+var svgContainer = d3.select("#map-container");
+var width = 1200;
+var height = 900; 
 // svg element
-const svg = svgContainer.append("svg")
+var svg = svgContainer.append("svg")
     .attr("width", width)
     .attr("height", height);
-
-
 //projecting data from 3d (earth) to a 2d (map)
-const projection = d3.geoMercator()
-    .scale(150)
+var projection = d3.geoMercator()
+    .scale(120)
+  //.center([0,20])
     .translate([width / 2, height / 2]);
-
 //convert geographic data into SVG path data
-const path = d3.geoPath().projection(projection);
-
+var path = d3.geoPath().projection(projection);
 //allow zooming on the map
 function zoomed(event) {
     svg.selectAll("path")  
         .attr("transform", event.transform);  
 }
-const zoom = d3.zoom()
+var zoom = d3.zoom()
     .scaleExtent([1, 10]) 
     .translateExtent([[0, 0], [width, height]]) 
     .on("zoom", zoomed);
+
 
 // load GeoJSON data (map)
 d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(function(world) {
     // load country data
     d3.json("\\data\\country.json").then(function(countryData) {
         // initialize color scale
-        const initialLanguage = "english";
-        const maxSongCount = d3.max(countryData, d => d[initialLanguage]);
-        const colorScale = d3.scaleSequential(d3.interpolateViridis)
-            .domain([0, maxSongCount]);
+        var initLang = "english";
+        var maxsongs = d3.max(countryData, d => d[initLang]);
+        var colorScale = d3.scaleSequential(d3.interpolateViridis)
+            .domain([0, maxsongs]);
 
         // adding an initial legend
         addLegend(colorScale);
-
         // select language
-        updateMap(initialLanguage);
-
+        updateMap(initLang);
         //add an event listner to the language selection dropdown tooltip
         d3.select("#language-select").on("change", function() {
-            const selectedLanguage = this.value;
-            updateMap(selectedLanguage);
+            var selectedLang = this.value;
+            updateMap(selectedLang);
         });
-
-        
-
-        //updating the map regarding the selected language
+                //updating the map regarding the selected language
         function updateMap(language) {
-            const colors = ["#C850C0","#FFCC70","#FFFF00","#4158D0" ];
+            var colors = ["#C850C0","#FFCC70","#FFFF00","#4158D0" ]; //just to be coherent with the rest of visualization of the project!
 
             // update the color scale wrt selected language distribution
-            const maxSongCount = d3.max(countryData, d => d[language]);
-            const colorScale = d3.scaleSequential(d3.interpolateViridis)
-                .domain([0, maxSongCount])
+            var maxsongs = d3.max(countryData, d => d[language]);
+            var colorScale = d3.scaleSequential(d3.interpolateViridis)
+                .domain([0, maxsongs])
                 .range(colors);
 
             //updating tooltip songs list
-            const tooltip = document.getElementById("song-tooltip");                                        
-            const tooltipHeader = document.getElementById("tooltip-header");
-            const tooltipSongList = document.getElementById("tooltip-song-list");
-            const closeButton = document.getElementById("close-list-button");
+            var tooltip = document.getElementById("song-tooltip");                                        
+            var tooltipHeader = document.getElementById("tooltip-header");
+            var tooltipSongList = document.getElementById("tooltip-song-list");
+            //adding a close button to allow closing the tooltip if no more needed
+            var closeButton = document.getElementById("close-button");
             closeButton.addEventListener("click", function() {
                 tooltip.classList.remove("active")
             });
-            //mapping data in country.json and the geo map
+            //mapping data in country.json and the geo map and varructing map and tooltips
             svg.selectAll("path")
                 .data(world.features)
                 .join("path")
                 .attr("d", path)
                 .style("fill", function(d) {
-                    const country = countryData.find(c => c.Country === d.properties.name); //get the country
+                    var country = countryData.find(c => c.Country === d.properties.name); //get the country
                     //console.log(country)
                     if (country) { //if country exists, mapping possible and found
-                        const languageCount = country[language]; 
-                        return languageCount !== undefined ? colorScale(languageCount) : "gray"; 
+                        var langCount = country[language]; 
+                        return langCount !== undefined ? colorScale(langCount) : "gray"; 
                     }
                     return "gray";
                 })
                 .on("click", function(event, d) { //when clicking on a country
   
                         if (d && d.properties && d.properties.name) {
-                            const countryName = d.properties.name; //selected country
-                            const country = countryData.find(c => c.Country === countryName);
+                            var countryName = d.properties.name; //selected country
+                            var country = countryData.find(c => c.Country === countryName);
+                            //after selecting a country, updating the different information in the toolips
                             if (country && country.songs) {
-                                //get distribution for the selected language
-                                const selectedLanguage = d3.select("#language-select").node().value;
-                                //filter songs according to the language
-                                const songsInLanguage = country.songs.filter(song => song[1] === selectedLanguage);
-                                //get song names and artists names
-                                const songNames = songsInLanguage.map(song => [song[0],song[2]]);
-                                //console.log(songNames)
+                                var selectedLang = d3.select("#language-select").node().value; //get the selected language
+                                var songsInLang = country.songs.filter(song => song[1] === selectedLang); //filter the songs to get only he songs in the selected language
+                                var artistsOfCountry = Array.from(new Set(songsInLang.map(song => song[2]))); //get all the artists in this country with songs in the selected language
+                                var artists = artistsOfCountry.filter((artistName, index, artists) => artists.indexOf(artistName) === index); //keep only unique artists, by keeping only first appearances names
+                
+                                tooltipHeader.textContent = `Artists with songs in ${countryName}:`; //set toolip title
+                                // define first level of tooltip to artists list
+                                var artistsHTML = artists.map(artistName => `
+                                    <li class="artist" name-artist="${artistName}">${artistName}</li>`
+                                ).join('');
+                                // add first level to tooltip
+                                tooltipSongList.innerHTML = artistsHTML;
+                                tooltip.classList.add("active");
 
-                                // update song list tooltip according to the language and country
-                                tooltipHeader.textContent = ` ${selectedLanguage.charAt(0).toUpperCase() + selectedLanguage.slice(1)} songs available in ${countryName}:`;
+                                //define back button to allow getting back from level 2 to level 1
+                                var backButton = document.querySelector('.back-button');
+                                
+                                backButton.addEventListener('click', (e) => {
+                                    e.stopPropagation(); // this allow to have the back button only for level 2 (not propagated to level 1 )
+                                    tooltipHeader.textContent = `Artists in ${countryName}`;
+                                    tooltipSongList.innerHTML = artistsHTML;
+                                    
+                                });
+                                
+                                // selecting artist and setting the levell 2 of the tooltip (songs of the selected artist)
+                                tooltipSongList.addEventListener('click', e => {
+                                    if (e.target.classList.contains('artist')) {
+                                        var selectedArtist = e.target.getAttribute('name-artist'); //get selected artist
+                                        var songsOfArtist = songsInLang.filter(song => song[2] === selectedArtist); //filtering the songs to get selected artist ones
+                                        tooltipHeader.textContent = `Songs by "${selectedArtist} "`; //setting title of level 2
+                                        var songListHTML = songsOfArtist.map(song => ` <li> <span class="song-name">${song[0].charAt(0).toUpperCase() + song[0].slice(1)}</span> </li>`).join('');
+                                        tooltipSongList.innerHTML = songListHTML;
+                                        backButton.style.display = 'block'; //display the back buttom for this level (2)
 
-                                // adding the songs to tooltip
-                                const songListHTML = songNames.map(songName => `
-                                        <li>
-                                            <span class="song-name">${songName[0].charAt(0).toUpperCase()}${songName[0].slice(1)}</span>
-                                            <span class="by"> by </span>${songName[1]}
-                                        </li>`
-                                    ).join('');
+                                    }
+                                });
 
-                                tooltipSongList.innerHTML = songListHTML;
-
-
-
-                            // position of the tooltip
-                            tooltip.style.left = event.pageX + "px";
-                            tooltip.style.top = event.pageY + "px";
-                            // activating/diplaying tooltip
-                            tooltip.classList.add("active");
+                                
                             }
 
                         }
+            });                     
+                
+            addLegend(colorScale); // adding and updating the legend when selecting another language
+        }
+        svg.call(zoom); //allow zooming
+        
+        var dragTooltip = document.querySelector('.draggable-tooltip');
+        
+        var dragg = false; // is the object being dragged
+        //initial positioon of the obejct (tooltip song)
+        var x = 0;
+        var y = 0;
+        //offset between cursor and object top left corner
+        var dx = 0;
+        var dy = 0;
 
-                           
-                    }) 
+        // event listner on the mouse to get the dragging 
 
- 
-                // update the legend when selecting another language
-                addLegend(colorScale);
-                }
-                svg.call(zoom);
+        //listen/track the mouse everywhere in the page
+        dragTooltip.addEventListener('mousemove',(e)=>{
+            if(!dragg){return;}
+            dx = e.clientX - x; //horizontal coord of the mouse
+            dy = e.clientY - y; //vertical
+            dragTooltip.style.transform = `translate(${dx}px, ${dy}px)` ;
+
+        });
+
+        // the case where the mouse is no longer on the web page
+        dragTooltip.addEventListener('mouseleave', () => {
+            if(dragg){
+                dragg = false;
+                dragTooltip.classList.remove('draggable');
+            }
+        });
+
+        //mouse is on the object/tooltip
+        dragTooltip.addEventListener('mousedown', (e) => {
+            dragg = true;
+            x = e.clientX - dx;
+            y = e.clientY - dy;
+            dragTooltip.classList.add('draggable');
+        });
+        
+        //mouse is released, no longer on the tooltip
+        dragTooltip.addEventListener('mouseup', () => {
+            dragg = false;
+           dragTooltip.classList.remove('draggable');
+        });
+    
+
 
     });
+
+
 });
 
-// Create a legend
+// creating legend for map
 function addLegend(colorScale) {
-    const legend = d3.select("#legend");
-
+    var legend = d3.select("#legend");
     //create legend using ticks from the color scale
-    const legendData = colorScale.ticks(15).reverse(); // Reverse the order of legendData
-    const legendWidth = 200;
-    const legendHeight = 15;
+    var legendData = colorScale.ticks(15).reverse(); // reverse the order of legendData
+    var legendWidth = 200;
+    var legendHeight = 15;
 
     // delete existing legend items
     legend.selectAll("*").remove();
